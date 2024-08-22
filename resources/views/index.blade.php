68ff7135c99fe6bef56f4a929f8431a540359142
Muhtasir Shafkat <msm.shafkat99@gmail.com>
2024-08-22 13:28:11 +0600

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Admin - @yield('title')</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    @vite('resources/css/app.css')
+
+    @yield('style') <!-- For including page-specific styles -->
+</head>
+<body class="bg-gray-100 text-gray-900">
+    <div class="min-h-screen flex flex-col">
+        <!-- Main Navigation -->
+        @yield('mainNav')
+
+        <!-- Main Content -->
+        <div class="flex-grow">
+            <div class="container mx-auto py-8">
+                @yield('mainContent')
+            </div>
+        </div>
+        <!-- End Main Content -->
+
+        <!-- Footer -->
+        <footer class="bg-white shadow-md">
+            <div class="container mx-auto py-4 flex justify-between items-center">
+                <span class="text-gray-600">&copy; System Resources Ltd. 2024</span>
+                <span class="text-gray-600">
+                    Maintained by <a href="https://srl.com.bd/" target="_blank" class="text-blue-500 hover:text-blue-700">System Resources Ltd</a>
+                </span>
+            </div>
+        </footer>
+    </div>
+
+
+    @yield('scripts')
+</body>
+</html>
